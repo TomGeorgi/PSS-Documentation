d10cd4f830410fa45b5c32701d61d9e8798e3614
Tom Georgi <Tom.Georgi@htwg-konstanz.de>
2019-03-12 21:28:21 +0100

    init docker branch

--- scripts/load_oscilloscope_class.py
+++
@@ -1,7 +0,0 @@
- def load_oscilloscope_interface(self):
-        replaced_path = str(self.__oscilloscope).replace(os.path.sep, '.')
-        scope = replaced_path.split('.')[-1]
-        module_string = replaced_path.rsplit('.', 1)[0]
-        module = importlib.import_module(module_string)
-        scope_class = getattr(module, scope)
-        return scope_class()
