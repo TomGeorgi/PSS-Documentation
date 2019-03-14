f66b50263ad61baadab9fb8a441e9ea79723f929
Tom Georgi <Tom.Georgi@htwg-konstanz.de>
2019-03-14 16:25:14 +0100

    Finished Chapter 3

--- scripts/receive_micbac.py
+++ scripts/receive_micbac.py
@@ -5,5 +5,4 @@ def received_msg(self, data_package):
 	information_str = '\n\t'
 	for dict_key in interface_obj.get_dict_information():
 		information_str += dict_key + ': ' + dict_information[dict_key] + '\n\t'
-
 	self.logger.info(information_str)
